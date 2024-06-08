76f69b45c7d16e1de4d2eed60ae6f2dbfc068ebe
midudzik <midudzik@d17-427-pc-01.ipa.ii.agh.edu.pl>
2024-06-08 12:14:43 +0200

    add basic commands in main

--- src/main/java/agh/g3/App.java
+++ src/main/java/agh/g3/App.java
@@ -8,6 +8,30 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        switch (args[0]) {
+            case "start":
+                System.out.println("starting task");
+                break;
+            case "stop":
+                System.out.println("stopping task");
+                break;
+            case "continue":
+                System.out.println("continue task");
+                break;
+            case "report":
+                System.out.println("printing report");
+                break;
+            case "list":
+                System.out.println("listing last logs");
+                break;
+            case "last":
+                System.out.println("last 5 unique tasks");
+                break;
+            case "help":
+                System.out.println("show help");
+                break;
+            default:
+                System.out.println("Use help command to see list of available commands");
+        }
     }
 }
