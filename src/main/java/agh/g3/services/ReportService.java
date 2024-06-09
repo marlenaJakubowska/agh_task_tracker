42bcdd66070a9b6ffcb7bead451874a106eeaa85
asiaKaplanska <kaplanska.joanna@gmail.com>
2024-06-09 14:15:31 +0200

    End first part of work

--- src/main/java/agh/g3/services/ReportService.java
+++ src/main/java/agh/g3/services/ReportService.java
@@ -6,13 +6,14 @@ import agh.g3.model.Status;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ReportService {
+    FileService fileService = new FileService();
+    List<Log> currentLogList = fileService.getLogList();
+    List<Log> logs = setTimeSpanLogs(readParameter("default"));
 
-    public Report generateReport(List<Log> logs) {
+    public Report generateReport() {
         Report report = new Report();
         Map<String, LocalDateTime> taskStartTimes = new HashMap<>();
 
@@ -53,5 +54,62 @@ public class ReportService {
         long minutes = (duration.toMinutes() % 60);
         long seconds = (duration.getSeconds() % 60);
         return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    private TimeSpan readParameter(String parametr) {
+        var command = parametr.toLowerCase();
+        var timeSpan = TimeSpan.DEFAULT;
+        switch (command) {
+            case "default" -> {return timeSpan;}
+            case "today" -> timeSpan = TimeSpan.TODAY;
+            case "yesterday" -> timeSpan = TimeSpan.YESTERDAY;
+            case "current week" -> timeSpan = TimeSpan.CURRENT_WEEK;
+            case "last week" -> timeSpan = TimeSpan.LAST_WEEK;
+        }
+        return timeSpan;
+    }
+
+    private List<Log> setTimeSpanLogs(TimeSpan timeSpan) {
+        var outList = new ArrayList<Log>();
+        var date = LocalDateTime.now().getDayOfYear();
+
+        switch (timeSpan) {
+            case DEFAULT -> outList.addAll(currentLogList);
+            case TODAY -> {
+                for (var log : currentLogList) {
+                    var logDate = log.getTime().getDayOfYear();
+                    if (logDate == date ) {
+                        outList.add(log);
+                    }
+                }
+            }
+            case YESTERDAY -> {
+                for (var log : currentLogList) {
+                    var logDate = log.getTime().getDayOfYear() + 1;
+                    if (logDate == date ) {
+                        outList.add(log);
+                    }
+                }
+            }
+            case CURRENT_WEEK -> {
+                for (var log : currentLogList) {
+                    var logDate = log.getTime().getDayOfYear();
+                    var dayOfWeek = log.getTime().getDayOfWeek().getValue();
+                    if (date - logDate < (7 - dayOfWeek)) {
+                        outList.add(log);
+                    }
+                }
+            }
+            case LAST_WEEK -> {
+                for (var log : currentLogList) {
+                    var logDate = log.getTime().getDayOfYear();
+                    var dayOfWeek = log.getTime().getDayOfWeek().getValue();
+                    if (date - logDate < (14 - dayOfWeek) && date - logDate > 7) {
+                        outList.add(log);
+                    }
+                }
+            }
+        }
+        return outList;
     }
 }
---
+++ src/main/java/agh/g3/services/TimeSpan.java
@@ -0,0 +1,11 @@
+package agh.g3.services;
+
+public enum TimeSpan {
+    DEFAULT,
+    TODAY,
+    YESTERDAY,
+    CURRENT_WEEK,
+    LAST_WEEK,
+    DATE,
+    FROM_TO
+}
